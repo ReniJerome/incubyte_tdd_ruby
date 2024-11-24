a2585c35f64cc394e25f9ed7b9c784f72047f25c
ReniJerome <66509947+ReniJerome@users.noreply.github.com>
2024-11-24 16:31:17 +0530

    Step 1- simple calculator

---
+++ StringCalculator.rb
@@ -0,0 +1,23 @@
+class StringCalculator
+  def add(nums)
+    arr = process_string_and_get_array(nums)
+    if arr.length == 0
+      puts "Array is empty. Sum is 0"
+      return 0
+    end
+    puts arr
+    sum = 0
+    arr.each do |arr_element|
+      sum = sum + arr_element.to_i
+    end
+    puts "Sum is #{sum}"
+  end
+  
+  def process_string_and_get_array(nums)
+    array = nums.split(',')
+    array
+  end
+  
+end
+
+StringCalculator.new.add('-2,-3,5,8')
